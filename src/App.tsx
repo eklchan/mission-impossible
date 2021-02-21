770c479f1bacadc0f4824e9c04e7421ca978c429
Edward Chan <edwardklc1993@gmail.com>
2021-02-21 19:46:32 +0000

    ESLINT configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useRef, useState } from 'react';
-import './App.css';
-import profiles from './mockdata.json';
-import CardItem from './components/CardItem';
-import cross from './assets/cross.png'
-import acceptedIcon from './assets/accepted.png'
-import rejectedIcon from './assets/rejected.png'
-import tick from './assets/tick.png'
-import mission from './assets/mission.png'
+import React, { useRef, useState } from "react";
+import "./App.css";
+import profiles from "./mockdata.json";
+import CardItem from "./components/CardItem";
+import cross from "./assets/cross.png";
+import acceptedIcon from "./assets/accepted.png";
+import rejectedIcon from "./assets/rejected.png";
+import tick from "./assets/tick.png";
+import mission from "./assets/mission.png";
 
 interface Profile {
   name: string;
@@ -15,97 +15,121 @@ interface Profile {
   image: string;
 }
 
-
 function App () {
   const [stack, setStack] = useState<Array<Profile>>(profiles);
   const [accepted, setAccepted] = useState<Array<Profile>>([]);
   const [rejected, setRejected] = useState<Array<Profile>>([]);
   const [likeState, setLikeState] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(true)
-  const counter = useRef(0)
+  const [loading, setLoading] = useState<boolean>(true);
+  const counter = useRef(0);
 
   const handleReject = () => {
-    setLikeState("reject")
-  }
+    setLikeState("reject");
+  };
 
-  //triggered if button pressed instead  
+  //triggered if button pressed instead
   const handleAccept = () => {
-    setLikeState("like")
-  }
+    setLikeState("like");
+  };
 
   //Updates Stack and stores swipe result
   const handleSwipeResult = (profile: Profile, result: boolean) => {
     // console.log(profile, result, "PROF")
-    let updatedStack: Array<Profile> = stack.filter((prof, i) => {
-      return (
-        i < stack.length - 1
-      )
-    })
+    const updatedStack: Array<Profile> = stack.filter((prof, i) => {
+      return i < stack.length - 1;
+    });
     if (stack.length - 1 === updatedStack.length) {
       if (result) {
-        setAccepted([...accepted, profile])
+        setAccepted([...accepted, profile]);
       } else if (result === false) {
-        setRejected([...rejected, profile])
+        setRejected([...rejected, profile]);
       }
-      setStack(updatedStack)
-      setLikeState("")
+      setStack(updatedStack);
+      setLikeState("");
     }
-
   };
 
   const renderStack = stack.map((profile, i) => {
-    let topCard: boolean = i === stack.length - 1;
+    const topCard: boolean = i === stack.length - 1;
 
     return (
-      <CardItem drag={topCard} profile={profile} handleSwipeResult={handleSwipeResult} key={i} likeState={likeState} setLoading={setLoading} loading={loading} counter={counter} length={profiles.length} />
-    )
-  })
+      <CardItem
+        drag={topCard}
+        profile={profile}
+        handleSwipeResult={handleSwipeResult}
+        key={i}
+        likeState={likeState}
+        setLoading={setLoading}
+        loading={loading}
+        counter={counter}
+        length={profiles.length}
+      />
+    );
+  });
 
   //RESET ALL STATE
   const handleReset = () => {
-    setLoading(true)
+    setLoading(true);
     counter.current = 0;
     setStack(profiles);
     setRejected([]);
     setAccepted([]);
-    setLikeState("")
-  }
+    setLikeState("");
+  };
 
   return (
     <div className="App">
-      <div className="frame" style={{ display: `${loading ? "none" : "flex"}` }}>
+      <div
+        className="frame"
+        style={{ display: `${loading ? "none" : "flex"}` }}
+      >
         <img src={mission} alt="logo" className="mission-logo"></img>
         {renderStack}
-        {!stack.length &&
+        {!stack.length && (
           <div className="results-wrap">
             <h1 className="results-header">RESULTS</h1>
             <div className="table-wrap">
               <div className="rejected">
-                <img src={rejectedIcon} alt="rejected" className="results-icon"></img>
+                <img
+                  src={rejectedIcon}
+                  alt="rejected"
+                  className="results-icon"
+                ></img>
                 {rejected.map((reject) => {
-                  return (
-                    <h1>{reject.name}</h1>
-                  )
+                  return <h1 key={reject.name}>{reject.name}</h1>;
                 })}
               </div>
               <div className="accepted">
-                <img src={acceptedIcon} alt="accepted" className="results-icon"></img>
+                <img
+                  src={acceptedIcon}
+                  alt="accepted"
+                  className="results-icon"
+                ></img>
                 {accepted.map((accept) => {
-                  return (
-                    <h1>{accept.name}</h1>
-                  )
+                  return <h1 key={accept.name}>{accept.name}</h1>;
                 })}
               </div>
             </div>
-            <button className="again-button" onClick={handleReset}>Start Again 🔄</button>
+            <button className="again-button" onClick={handleReset}>
+              Start Again 🔄
+            </button>
           </div>
-        }
-        {!!stack.length && <div className="button-wrap">
-          <button className="button" onClick={handleReject}><img src={cross} alt="cross" className="button-image" /></button>
-          <button className="button" onClick={handleAccept}><img src={tick} alt="tick" className="button-image" /></button>
-        </div>}
+        )}
+        {!!stack.length && (
+          <div className="button-wrap">
+            <button className="button" onClick={handleReject}>
+              <img src={cross} alt="cross" className="button-image" />
+            </button>
+            <button className="button" onClick={handleAccept}>
+              <img src={tick} alt="tick" className="button-image" />
+            </button>
+          </div>
+        )}
       </div>
-      <div className="frame" style={{ display: `${!loading ? "none" : "flex"}` }}>
+      <div
+        className="frame"
+        style={{ display: `${!loading ? "none" : "flex"}` }}
+      >
         <img src={mission} alt="logo" className="mission-logo"></img>
         <div className="spinner" />
       </div>
--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,8 +1,8 @@
-import React, { useRef, useEffect, useState } from "react"
-import { useAnimation, useMotionValue, motion } from 'framer-motion';
-import './CardItem.css'
-import reject from '../assets/reject.png'
-import pass from '../assets/pass.png'
+import React, { useRef, useEffect, useState } from "react";
+import { useAnimation, useMotionValue, motion } from "framer-motion";
+import "./CardItem.css";
+import reject from "../assets/reject.png";
+import pass from "../assets/pass.png";
 
 interface Profile {
   name: string;
@@ -11,95 +11,112 @@ interface Profile {
   image: string;
 }
 
-const CardItem = ({ profile, handleSwipeResult, drag, likeState, setLoading, counter, length, loading }: { likeState: string, profile: Profile, drag: boolean, handleSwipeResult: Function, loading: boolean, setLoading: Function, length: number, counter: any }) => {
-  const cardEl: any = useRef(null)
-  const x = useMotionValue(0)
-  const control = useAnimation()
-  const [initialPosition, setInitialPosition] = useState<number>(0)
-  const [shiftedPosition, setShiftedPosition] = useState<number>(0)
-  const [constrain, setConstrain] = useState<boolean>(true)
-  const [direction, setDirection] = useState<string>()
-  const [velocity, setVelocity] = useState<number>(0)
+const CardItem = ({
+  profile,
+  handleSwipeResult,
+  drag,
+  likeState,
+  setLoading,
+  counter,
+  length,
+  loading,
+}: {
+  likeState: string;
+  profile: Profile;
+  drag: boolean;
+  handleSwipeResult: any;
+  loading: boolean;
+  setLoading: any;
+  length: number;
+  counter: any;
+}) => {
+  const cardEl: any = useRef(null);
+  const x = useMotionValue(0);
+  const control = useAnimation();
+  const [initialPosition, setInitialPosition] = useState<number>(0);
+  const [shiftedPosition, setShiftedPosition] = useState<number>(0);
+  const [constrain, setConstrain] = useState<boolean>(true);
+  const [direction, setDirection] = useState<string>();
+  const [velocity, setVelocity] = useState<number>(0);
 
   const getDirection = () => {
-    return velocity >= 1 ? "right" : velocity <= -1 ? "left" : undefined
-  }
+    return velocity >= 1 ? "right" : velocity <= -1 ? "left" : undefined;
+  };
 
   const getMovementState = () => {
-    setVelocity(x.getVelocity())
-    setDirection(getDirection())
-  }
+    setVelocity(x.getVelocity());
+    setDirection(getDirection());
+  };
 
   //Swipe left or Right?
   const getResult = (childNode: any, parentNode: any) => {
-    const child = childNode.getBoundingClientRect()
-    const parent = parentNode.getBoundingClientRect()
-    let result =
+    const child = childNode.getBoundingClientRect();
+    const parent = parentNode.getBoundingClientRect();
+    const result =
       parent.left >= child.right
         ? false
         : parent.right <= child.left
           ? true
-          : undefined
-    return result
-  }
+          : undefined;
+    return result;
+  };
 
   //Speed to trigger swipe
   const flyAway = (min: number) => {
     const flyAwayDistance = (direction: string) => {
       const parentWidth = cardEl.current.parentNode.getBoundingClientRect()
-        .width
-      const childWidth = cardEl.current.getBoundingClientRect().width
+        .width;
+      const childWidth = cardEl.current.getBoundingClientRect().width;
       return direction === "left"
         ? -parentWidth / 2 - childWidth / 2
-        : parentWidth / 2 + childWidth / 2
-    }
+        : parentWidth / 2 + childWidth / 2;
+    };
 
     if (direction && Math.abs(velocity) > min) {
-      setConstrain(false)
+      setConstrain(false);
       control.start({
-        x: flyAwayDistance(direction)
-      })
+        x: flyAwayDistance(direction),
+      });
     }
-  }
+  };
 
   //Event listener for card drag
   useEffect(() => {
     const unsubscribe = x.onChange(() => {
-      const childNode = cardEl.current
-      const parentNode = cardEl.current.parentNode
-      const result = getResult(childNode, parentNode)
-      setShiftedPosition(childNode.getBoundingClientRect().x - initialPosition)
+      const childNode = cardEl.current;
+      const parentNode = cardEl.current.parentNode;
+      const result = getResult(childNode, parentNode);
+      setShiftedPosition(childNode.getBoundingClientRect().x - initialPosition);
       if (result !== undefined) {
-        handleSwipeResult(profile, result)
+        handleSwipeResult(profile, result);
       }
-    })
-    return () => unsubscribe()
-  })
+    });
+    return () => unsubscribe();
+  });
 
   //Initial position of Card
   useEffect(() => {
-    setInitialPosition(cardEl.current.getBoundingClientRect().x)
-  }, [loading])
+    setInitialPosition(cardEl.current.getBoundingClientRect().x);
+  }, [loading]);
 
   //Handling Button press
   useEffect(() => {
     setTimeout(() => {
       if (likeState === "like") {
-        handleSwipeResult(profile, true)
+        handleSwipeResult(profile, true);
       } else if (likeState === "reject") {
-        handleSwipeResult(profile, false)
+        handleSwipeResult(profile, false);
       }
     }, 450);
-  }, [likeState])
+  }, [likeState]);
 
-
-  //For loading spinner  
+  //For loading spinner
   const handleLoaded = () => {
     counter.current += 1;
     if (counter.current >= length) {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   return (
     <motion.div
@@ -111,24 +128,55 @@ const CardItem = ({ profile, handleSwipeResult, drag, likeState, setLoading, cou
       style={{ x }}
       onDrag={getMovementState}
       onDragEnd={() => flyAway(500)}
-      drag={drag}>
-      <div className={`carditem-wrapper ${drag && likeState}`} style={{ transform: `rotate(${shiftedPosition / 50}deg) scale(${drag ? 1.006 : 1})` }}>
-        <img onLoad={handleLoaded} className="carditem-image" src={profile.image} alt={profile.name} draggable="false"></img>
+      drag={drag}
+    >
+      <div
+        className={`carditem-wrapper ${drag && likeState}`}
+        style={{
+          transform: `rotate(${shiftedPosition / 50}deg) scale(${drag ? 1.006 : 1
+            })`,
+        }}
+      >
+        <img
+          onLoad={handleLoaded}
+          className="carditem-image"
+          src={profile.image}
+          alt={profile.name}
+          draggable="false"
+        ></img>
         <div className="status-wrapper">
-          <img src={pass} alt="pass" style={{ opacity: `${shiftedPosition / 400}` }} className="carditem-icon"></img>
-          <h2 style={{ opacity: `${shiftedPosition / 400}`, color: 'lightgreen' }}>LIKE</h2>
+          <img
+            src={pass}
+            alt="pass"
+            style={{ opacity: `${shiftedPosition / 400}` }}
+            className="carditem-icon"
+          ></img>
+          <h2
+            style={{ opacity: `${shiftedPosition / 400}`, color: "lightgreen" }}
+          >
+            LIKE
+          </h2>
         </div>
         <div className="status-wrapper">
-          <img src={reject} alt="reject" style={{ opacity: `${-shiftedPosition / 400}` }} className="carditem-icon"></img>
-          <h2 style={{ opacity: `${-shiftedPosition / 400}`, color: 'red' }}>NOPE</h2>
+          <img
+            src={reject}
+            alt="reject"
+            style={{ opacity: `${-shiftedPosition / 400}` }}
+            className="carditem-icon"
+          ></img>
+          <h2 style={{ opacity: `${-shiftedPosition / 400}`, color: "red" }}>
+            NOPE
+          </h2>
         </div>
         <div className="carditem-textwrap">
-          <h1 className="carditem-name">{profile.name} {profile.age}</h1>
+          <h1 className="carditem-name">
+            {profile.name} {profile.age}
+          </h1>
           <h2 className="carditem-description">{profile.description}</h2>
         </div>
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
 export default CardItem;
